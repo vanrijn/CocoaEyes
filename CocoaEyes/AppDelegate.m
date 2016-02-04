da99bdf58a96ccd62052d44bfbb985a1f3924b02
Jason 'vanRijn' Kasper <vR@movingparts.net>
2016-02-03 22:59:54 -0500

    baby steps. events get logged when app is not active

--- CocoaEyes/AppDelegate.m
+++ CocoaEyes/AppDelegate.m
@@ -10,16 +10,26 @@
 
 @interface AppDelegate ()
 
+@property (readwrite, retain, nonatomic) id eventHandler;
+
 @end
 
 @implementation AppDelegate
 
+@synthesize eventHandler;
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    // XXX Apparently this doesn't work when the CocoaEyes application is active?
+    // Also doesn't work in Mission Control view.
+    eventHandler = [NSEvent addGlobalMonitorForEventsMatchingMask:NSMouseMovedMask handler:^(NSEvent * mouseEvent) {
+        NSLog(@"Mouse moved: %@", NSStringFromPoint([mouseEvent locationInWindow]));
+    }];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+    [NSEvent removeMonitor:eventHandler];
 }
 
 @end
